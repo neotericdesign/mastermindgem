b9e60c78a4916fc46d216e7734d87ffa1f009355
Matthew Cowie <macowie@gmail.com>
2013-04-03 10:57:38 -0500

    Refactor feedback down to single public method

--- lib/mastermind/feedback.rb
+++ lib/mastermind/feedback.rb
@@ -4,55 +4,36 @@ module Mastermind
 
     def initialize(code, guess)
       @code  = code.split(' ')
-      @guess = guess
+      @guess = guess.split(' ')
     end
 
     def pegs
       returned_pegs = no_pegs
 
-      unless no_correct_colors?
-        white_pegs.each_with_index do |white_peg, i|
-          returned_pegs[i] = white_peg
-        end
-
-       black_pegs.each_with_index do |black_peg, i|
-          returned_pegs[i] = black_peg
-        end
-      end
+      add_pegs(returned_pegs)
 
       returned_pegs.join(' ')
-    end
-
-    def no_correct_colors?
-      not code.any? do |c|
-        guess.include?(c)
-      end
-    end
-
-    def white_pegs
-      pegs = []
-
-      code.each do |c|
-        pegs << 'W' if guess.include?(c)
-      end
-
-      pegs
-    end
-
-    def black_pegs
-      pegs = []
-
-
-      guess.split(' ').each_with_index do |g,i|
-        pegs << 'B' if code[i] == g
-      end
-
-      pegs
     end
 
     private
     def no_pegs
       %w(- - - -)
+    end
+    def add_pegs(pegs)
+      black_pegs = []
+      white_pegs = []
+
+      guess.each_with_index do |g, i|
+        if code[i] == g
+          black_pegs << 'B'
+        elsif code.include?(g)
+          white_pegs << 'W'
+        end
+      end
+
+      (black_pegs + white_pegs).each_with_index do |peg, i|
+        pegs[i] = peg
+      end
     end
   end
 end
--- spec/mastermind/feedback_spec.rb
+++ spec/mastermind/feedback_spec.rb
@@ -31,51 +31,5 @@ module Mastermind
         end
       end
     end
-
-    describe "#no_correct_colors?" do
-      it "returns true for no correct colors" do
-        feedback = Feedback.new(code, incorrect_guess)
-
-        feedback.should be_no_correct_colors
-      end
-
-      it "returns false for any correct colors" do
-        feedback = Feedback.new(code, "Y P W C")
-
-        feedback.should_not be_no_correct_colors
-      end
-    end
-
-    describe "#white_pegs" do
-      it "returns a W for each correct color" do
-        feedback = Feedback.new(code, "Y P W C")
-        pegs     = feedback.white_pegs
-
-        pegs.should == ["W"]
-      end
-
-      it "returns empty array  for no correct colors" do
-        feedback = Feedback.new(code, "O W P C")
-        pegs     = feedback.white_pegs
-
-        pegs.should == []
-      end
-    end
-    describe "#black_pegs" do
-      it "returns a B for each correct position" do
-        feedback = Feedback.new(code, "R P W C")
-        pegs     = feedback.black_pegs
-
-        pegs.should == ["B"]
-      end
-
-      it "returns empty array for no correct position" do
-        feedback = Feedback.new(code, "O W P C")
-        pegs     = feedback.black_pegs
-
-        pegs.should == []
-      end
-    end
-
   end
 end
